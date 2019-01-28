fce4a3452a25356d84835d8dadc84bc342f37eab
avanaparthi86 <47083314+avanaparthi86@users.noreply.github.com>
2019-01-28 14:47:49 +0100

    Add files via upload

---
+++ MicroApp/Controllers/RadioController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MicroApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RadioController : ControllerBase
+    {
+        private RadioSystem objRadio = new RadioSystem();
+        // GET: api/Radio
+        [HttpGet]
+        public List<RadioSystem> GetAllRadioSystems()
+        {
+            return objRadio.GetAlRadioSystems();
+        }
+
+        // GET: api/Radio/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            var returnValue = string.Empty;
+            var foundItem = objRadio.RadioSystems.Find(item => item.RadioId == id);
+            if (foundItem != null)
+            {
+                returnValue = foundItem.AllowedLocations?[foundItem.AllowedLocations.Count - 1];
+            }
+            else
+            {
+                returnValue = "undefiner/unknown";
+            }
+            return returnValue;
+        }
+
+        [HttpPost]
+        public List<RadioSystem> CreateRadioSystem([FromBody] RadioSystem radioSystem)
+        {
+            objRadio.AddRadioSystem(radioSystem);
+            return objRadio.RadioSystems;
+        }
+
+        [HttpPut("{id}")]
+        public List<RadioSystem> UpdateRadioSystem(int id, [FromBody] string location)
+        {
+            var itemsList = objRadio.RadioSystems;
+            var foundItem = itemsList.Find(item => item.RadioId == id);
+            if (foundItem != null)
+            {
+                if (foundItem.AllowedLocations.Contains(location))
+                {
+                    foundItem.AllowedLocations.Add(location);
+                }
+            }
+            return objRadio.RadioSystems;
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var foundItem = objRadio.RadioSystems.Find(item => item.RadioId == id);
+            objRadio.RadioSystems.Remove(foundItem);
+        }
+    }
+}
---
+++ MicroApp/Models/RadioSystem.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Protocols;
+
+namespace MicroApp.Models
+{
+    public class RadioSystem
+    {
+        public int RadioId { get; set; }
+        public string RadioAlias { get; set; }
+        public List<string> AllowedLocations { get; set; }
+
+        public List<RadioSystem> RadioSystems { get; set; }
+
+        public void AddRadioSystem(RadioSystem system)
+        {
+            RadioSystems.Add(system);
+        }
+
+        public List<RadioSystem> GetAlRadioSystems()
+        {
+            var items = new List<RadioSystem>
+            {
+                new RadioSystem
+                {
+                    RadioId = 100,
+                    RadioAlias = "Radio100",
+                    AllowedLocations = new List<string> {"CPH-1", "CPH2"}
+                },
+                new RadioSystem
+                {
+                    RadioId = 101,
+                    RadioAlias = "Radio101",
+                    AllowedLocations = new List<string> {"CPH-1", "CPH2", "CPH3" }
+                }
+            };
+            if (RadioSystems != null) RadioSystems.AddRange(items);
+            return items;
+        }
+    }
+}
