a3a64ed916dba0eb1e0c58448b427b8cc625c04a
avanaparthi86 <47083314+avanaparthi86@users.noreply.github.com>
2019-02-06 07:53:23 +0100

    Update RadioController.cs

--- MicroApp/Controllers/RadioController.cs
+++ MicroApp/Controllers/RadioController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -25,11 +25,20 @@ namespace MicroApp.Controllers
         public string Get(int id)
         {
             var returnValue = string.Empty;
-            objRadio.RadioSystems= objRadio.GetAlRadioSystems();
+            objRadio.RadioSystems = objRadio.GetAlRadioSystems();
             var foundItem = objRadio.RadioSystems.Find(item => item.RadioId == id);
             if (foundItem != null)
             {
-                returnValue = foundItem.AllowedLocations?[foundItem.AllowedLocations.Count - 1];
+                if (id == 100)
+                    returnValue = foundItem.AllowedLocations?[0];
+                else
+                    returnValue = foundItem.AllowedLocations ? [foundItem.AllowedLocations.Count - 1];
+                //for (int i = 0; i < foundItem.AllowedLocations.Count; i++)
+                //{
+                //    if (!string.IsNullOrEmpty(returnValue))
+                //        returnValue += ",";
+                //    returnValue += foundItem.AllowedLocations[i];
+                //}
             }
             else
             {
@@ -39,33 +48,45 @@ namespace MicroApp.Controllers
         }
 
         [HttpPost]
-        public List<RadioSystem> CreateRadioSystem([FromBody] RadioSystem radioSystem)
+        public List<RadioSystem> CreateRadioSystem([FromBody]RadioSystem[] radioSystem)
         {
-            objRadio.AddRadioSystem(radioSystem);
+            objRadio.RadioSystems = new List<RadioSystem>(radioSystem.Length);
+            for (int i = 0; i < radioSystem.Length; i++)
+                objRadio.AddRadioSystem(radioSystem[i]);
             return objRadio.RadioSystems;
         }
 
         [HttpPut("{id}")]
-        public List<RadioSystem> UpdateRadioSystem(int id, [FromBody] string location)
+        public string UpdateRadioSystem(int id, [FromBody] string location)
         {
-            var itemsList = objRadio.RadioSystems;
+            string response = string.Empty;
+            var itemsList = objRadio.GetAlRadioSystems();
+            objRadio.RadioSystems = itemsList;
             var foundItem = itemsList.Find(item => item.RadioId == id);
             if (foundItem != null)
-            {
+            {                
                 if (foundItem.AllowedLocations.Contains(location))
                 {
+                    response = "Accepted";
                     foundItem.AllowedLocations.Add(location);
                 }
+                else
+                    response = "Denied";
             }
-            return objRadio.RadioSystems;
+            return response;
         }
 
         // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public List<RadioSystem> Delete(int id)
         {
+            var itemsList = objRadio.GetAlRadioSystems();
+            objRadio.RadioSystems = itemsList;
             var foundItem = objRadio.RadioSystems.Find(item => item.RadioId == id);
             objRadio.RadioSystems.Remove(foundItem);
+            return objRadio.RadioSystems;
         }
     }
+
+
 }
