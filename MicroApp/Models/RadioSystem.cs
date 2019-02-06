6a667e6744cb82b4e8b71231f20f5afc3560978b
avanaparthi86 <47083314+avanaparthi86@users.noreply.github.com>
2019-02-06 07:54:30 +0100

    Update RadioSystem.cs

--- MicroApp/Models/RadioSystem.cs
+++ MicroApp/Models/RadioSystem.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -15,29 +15,58 @@ namespace MicroApp.Models
         public List<RadioSystem> RadioSystems { get; set; }
 
         public void AddRadioSystem(RadioSystem system)
-        {
+        {           
             RadioSystems.Add(system);
         }
 
         public List<RadioSystem> GetAlRadioSystems()
         {
+            //****************** Scenario 1
+            #region
             var items = new List<RadioSystem>
             {
                 new RadioSystem
                 {
                     RadioId = 100,
                     RadioAlias = "Radio100",
-                    AllowedLocations = new List<string> {"CPH-1", "CPH2"}
+                    AllowedLocations = new List<string> {"CPH-1", "CPH-2"}
                 },
                 new RadioSystem
                 {
                     RadioId = 101,
                     RadioAlias = "Radio101",
-                    AllowedLocations = new List<string> {"CPH-1", "CPH2", "CPH3" }
+                    AllowedLocations = new List<string> {"CPH-1", "CPH-2", "CPH-3" }
                 }
             };
+            #endregion
+            //****************** Scenario 2
+            #region
+            //var items = new List<RadioSystem>
+            //{
+            //    new RadioSystem
+            //    {
+            //        RadioId = 102,
+            //        RadioAlias = "Radio102",
+            //        AllowedLocations = new List<string> {"CPH-1", "CPH-3"}
+            //    }
+            //};
+            #endregion
+            //******************Scenario 3
+            #region
+            //var items = new List<RadioSystem>
+            //{
+            //    new RadioSystem
+            //    {
+            //        RadioId = 101,
+            //        RadioAlias = "Radio101",
+            //        AllowedLocations = new List<string> {"CPH-1", "CPH-2", "CPH-3"}
+            //    }
+            //};
+            #endregion
             if (RadioSystems != null) RadioSystems.AddRange(items);
             return items;
         }
     }
+
+
 }
